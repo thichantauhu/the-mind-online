39d8e30b5f6aba5988e87ff5c47e67d21e4b08ac
thichantauhu <yuki985400@gmail.com>
2026-09-15 13:33:12 +0700

    Configure The Mind rules

---
+++ public/config.js
@@ -0,0 +1,14 @@
+window.GAME_CONFIG = {
+  name: 'The Mind',
+  minPlayers: 2,
+  maxPlayers: 4,
+  maxBots: 4,
+  rules: [
+    'Cả đội cùng đánh các lá số 1–100 theo thứ tự tăng dần.',
+    'Không được nói, cho xem bài hoặc ra hiệu về giá trị lá bài.',
+    'Mỗi người tự chọn thời điểm đánh bài của mình.',
+    'Đánh sai thứ tự: mất 1 mạng; các lá nhỏ hơn lá vừa đánh sẽ bị bỏ.',
+    'Phi tiêu: cả đội đồng ý thì mỗi người bỏ lá thấp nhất đang có.',
+    'Hoàn thành toàn bộ cấp độ trước khi hết mạng để chiến thắng.'
+  ]
+};
